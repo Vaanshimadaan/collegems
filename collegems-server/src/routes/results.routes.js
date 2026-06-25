8c23e25851e244b59dbb098d516d05bd4c094da3
Aritro Bag <aritrobag362@gmail.com>
2026-06-26 04:17:01 +0530

    Added a secure way to publish results

--- collegems-server/src/routes/results.routes.js
+++ collegems-server/src/routes/results.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createResult, getResults, publishResult } from '../controllers/results.controller.js';
+import { createResult, getResults, publishResult, publishPreview, publishAll } from '../controllers/results.controller.js';
 import { protect } from "../middlewares/auth.middleware.js";
 import { checkDataLock } from "../middlewares/dataLock.middleware.js";
 import { auditAction } from "../middlewares/audit.middleware.js";
@@ -8,4 +8,6 @@ const router = express.Router();
 router.get("/my", protect, getResults);
 router.post('/create', protect, checkDataLock('results'), auditAction('CREATE_RESULT', 'Results'), createResult);
 router.put('/:id/publish', protect, checkDataLock('results'), auditAction('PUBLISH_RESULT', 'Results'), publishResult);
+router.post('/publish-preview', protect, checkDataLock('results'), publishPreview);
+router.post('/publish-all', protect, checkDataLock('results'), auditAction('BULK_PUBLISH_RESULTS', 'Results'), publishAll);
 export default router;
