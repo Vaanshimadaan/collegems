552a58967b01c7e09aaffc50ae5faeb42dfd09c2
Suchita <jsuchits923@gmail.com>
2026-07-01 02:17:58 +0530

    feature: add visitor management backend APIs

--- collegems-server/src/controllers/visitors.controller.js
+++ collegems-server/src/controllers/visitors.controller.js
@@ -54,3 +54,36 @@ export const markExit = async (req, res) => {
     });
   }
 };
+
+
+
+export const updatedVisitor = async (req, res) => {
+  try {
+    const visitor = await Visitor.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!visitor) {
+      return res.status(404).json({
+        success: false,
+        message: "Visitor not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Visitor updated successfully",
+      visitor,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- collegems-server/src/routes/visitors.routes.js
+++ collegems-server/src/routes/visitors.routes.js
@@ -3,12 +3,19 @@ import {
   addVisitor,
   getVisitors,
   markExit,
-} from "../controllers/visitors.controller.js";
+  updatedVisitor
+}from "../controllers/visitors.controller.js";
+
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { authorize } from "../middlewares/role.middleware.js";
+
+
 
 const router = express.Router();
 
-router.post("/", addVisitor);
-router.get("/", getVisitors);
-router.put("/exit/:id", markExit);
+router.post("/", authenticate, authorize("hod"),addVisitor);
+router.get("/",authenticate, authorize("hod"), getVisitors);
+router.put("/exit/:id",authenticate, authorize("hod"), markExit);
+router.put("/:id", authenticate, authorize("hod"),updatedVisitor);
 
 export default router;
